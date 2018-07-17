baeeb19ab36bc1e436173709fb5403d2cb79f785
Ben <ben.kyle.robinson@gmail.com>
2018-07-17 07:49:55 -0400

    adding plotting

--- initial_exploration.py
+++ initial_exploration.py
@@ -1,10 +1,14 @@
-#See https://github.com/RomelTorres/alpha_vantage/blob/develop/README.md
-
 from alpha_vantage.timeseries import TimeSeries
 import json
 import config
 from pprint import pprint
-ts = TimeSeries(key=config.av_key, output_format = 'pandas')
-data, meta_data = ts.get_daily('RUBI')
+import matplotlib.pyplot as plt
 
-pprint(data.head(180))
+ts = TimeSeries(key=config.av_key, output_format = 'pandas')
+data, meta_data = ts.get_daily(symbol='RUBI', outputsize='full')
+data['4. close'].plot()
+plt.title('Closing Prices for RUBI Stock')
+plt.show()
+
+
+
